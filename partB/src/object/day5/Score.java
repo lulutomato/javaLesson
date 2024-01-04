5fb3a5c5ae92e87f3ce7c4cba1bfc99a6f3d4993
chajungho <slomo.littlecha@gmail.com>
2024-01-04 17:46:50 +0900

    object.day5

--- partB/src/object/day5/Score.java
+++ partB/src/object/day5/Score.java
@@ -5,11 +5,12 @@ import java.util.Arrays;
 //학생들의 성적을 객체로 만들어 줄 클래스 입니다.
 public class Score {
   
-
+      //인스터스 필드
       private String name; // 학생이름
       private int grade; // 학년
       private int[] jumsues; // 점수들이 저장될 배열
                             // 배열의 크기는 학년마다 다를 수 있음.
+      //인스터스 메소드 : 인스턴스 필드를 사용하는 처리를 합니다.
 
       //getter
       public String getname(){
@@ -35,7 +36,7 @@ public class Score {
       public void setjumsues(int[] j1){
         jumsues = j1;
       }
-      
+      //현재 객체의 점수의 합계(개인총점)를 리턴하는 sum()메소드
       public int sum(){
           int sum =0;
           for(int i=0;i<jumsues.length;i++){
@@ -48,18 +49,24 @@ public class Score {
           double temp = 0;  // temp를 이용해서 위에 sum을 구한걸 가져옴
           temp = (double)sum()/jumsues.length;//double 로 캐스팅
           return temp;
-          
+          //return (double)sum()/jumsues.length;
         }
 
         public void printData(){
           System.out.println("이름: " + name + "\n학년 : "+ grade+ "\n과목 점수=" + Arrays.toString(jumsues));
 
         }
-          
+        // public void printScore(){
+        // System.out.println("총점 :" +sum());
+        // System.out.println("과목 개수 :" + jumsues.length);
+        // System.out.println("평균:" + average() );
+
+
+        }  
           
 
 
-        }
+        
 
 
 
